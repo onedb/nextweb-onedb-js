e275385e541042e8d77461e8b3fcec95928b6627
mxro <mxro@nowhere.com>
2013-06-18 17:50:09 +1200

    enhancing api to allow third party persistence connections to be defined when creating a session.

--- src/main/java/com/ononedb/nextweb/js/OnedbNextwebJsEngineImpl.java
+++ src/main/java/com/ononedb/nextweb/js/OnedbNextwebJsEngineImpl.java
@@ -5,6 +5,7 @@ import io.nextweb.common.LocalServer;
 import io.nextweb.engine.Capability;
 import io.nextweb.engine.Factory;
 import io.nextweb.engine.StartServerCapability;
+import io.nextweb.engine.persistence.PersistenceConnection;
 import io.nextweb.fn.ExceptionListener;
 import io.nextweb.fn.ExceptionResult;
 import io.nextweb.js.NextwebJs;
@@ -21,141 +22,156 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.ononedb.nextweb.common.H;
 import com.ononedb.nextweb.common.OnedbFactory;
+import com.ononedb.nextweb.common.Wrappers;
 import com.ononedb.nextweb.js.internal.OnedbJsFactory;
 import com.ononedb.nextweb.plugins.DefaultPluginFactory;
 
 public class OnedbNextwebJsEngineImpl implements OnedbNextwebEngineJs {
 
-	private CoreDsl dsl;
-	private final ExceptionManager exceptionManager;
-	private final JsFactory jsFactory;
-	private StartServerCapability startServerCapability;
+    private CoreDsl dsl;
+    private final ExceptionManager exceptionManager;
+    private final JsFactory jsFactory;
+    private StartServerCapability startServerCapability;
 
-	public static OnedbNextwebJsEngineImpl init() {
-		final OnedbNextwebJsEngineImpl engine = new OnedbNextwebJsEngineImpl();
-		NextwebJs.injectEngine(JsNextwebEngine.wrap(engine));
-		return engine;
-	}
+    public static OnedbNextwebJsEngineImpl init() {
+        final OnedbNextwebJsEngineImpl engine = new OnedbNextwebJsEngineImpl();
+        NextwebJs.injectEngine(JsNextwebEngine.wrap(engine));
+        return engine;
+    }
 
-	public static OnedbNextwebJsEngineImpl assertInitialized() {
-		if (NextwebJs.getEngine() == null
-				|| (!(NextwebJs.getEngine() instanceof JsNextwebEngine))) {
-			return init();
-		}
+    public static OnedbNextwebJsEngineImpl assertInitialized() {
+        if (NextwebJs.getEngine() == null
+                || (!(NextwebJs.getEngine() instanceof JsNextwebEngine))) {
+            return init();
+        }
 
-		return (OnedbNextwebJsEngineImpl) NextwebJs.getEngine().getEngine();
-	}
+        return (OnedbNextwebJsEngineImpl) NextwebJs.getEngine().getEngine();
+    }
 
-	private CoreDsl assertDsl() {
-		if (dsl != null) {
-			return dsl;
-		}
+    private CoreDsl assertDsl() {
+        if (dsl != null) {
+            return dsl;
+        }
 
-		final GwtRemoteServiceAsync gwtService = GWT
-				.create(GwtRemoteService.class);
+        final GwtRemoteServiceAsync gwtService = GWT
+                .create(GwtRemoteService.class);
 
-		((ServiceDefTarget) gwtService)
-				.setServiceEntryPoint("/servlets/v01/gwtrpc");
+        ((ServiceDefTarget) gwtService)
+                .setServiceEntryPoint("/servlets/v01/gwtrpc");
 
-		dsl = OneGwt.init(gwtService, "");
+        dsl = OneGwt.init(gwtService, "");
 
-		dsl.getDefaults().getSettings()
-				.setDefaultBackgroundListener(new BackgroundListener() {
+        dsl.getDefaults().getSettings()
+                .setDefaultBackgroundListener(new BackgroundListener() {
 
-					@Override
-					public void onBackgroundException(final Object operation,
-							final Throwable t, final Throwable origin) {
-						throw new RuntimeException("Exception: "
-								+ t.getMessage() + " for operation: ["
-								+ operation + "] originating from: [" + origin
-								+ "].", t);
-					}
-				});
+                    @Override
+                    public void onBackgroundException(final Object operation,
+                            final Throwable t, final Throwable origin) {
+                        throw new RuntimeException("Exception: "
+                                + t.getMessage() + " for operation: ["
+                                + operation + "] originating from: [" + origin
+                                + "].", t);
+                    }
+                });
 
-		return dsl;
-	}
+        return dsl;
+    }
 
-	@Override
-	public Session createSession() {
+    @Override
+    public Session createSession() {
 
-		final CoreDsl dsl = assertDsl();
+        final CoreDsl dsl = assertDsl();
 
-		return getOnedbFactory().createSession(this, dsl.createClient());
-	}
+        return getOnedbFactory().createSession(this, dsl.createClient());
+    }
 
-	@Override
-	public ExceptionManager getExceptionManager() {
+    @Override
+    public boolean hasPersistedReplicationCapability() {
 
-		return exceptionManager;
-	}
+        return true;
+    }
 
-	@Override
-	public OnedbFactory getOnedbFactory() {
-		return new OnedbJsFactory();
-	}
+    @Override
+    public Session createSession(final PersistenceConnection connection) {
+        final CoreDsl dsl = assertDsl();
 
-	@Override
-	public Factory getFactory() {
-		return new OnedbJsFactory();
-	}
+        return getOnedbFactory().createSession(this,
+                dsl.createClient(Wrappers.wrap(connection)));
+    }
 
-	public OnedbNextwebJsEngineImpl() {
-		super();
-		this.exceptionManager = getOnedbFactory().createExceptionManager(null);
-		this.exceptionManager.catchExceptions(new ExceptionListener() {
+    @Override
+    public ExceptionManager getExceptionManager() {
 
-			@Override
-			public void onFailure(final ExceptionResult r) {
-				throw new RuntimeException("Unhandled exception: "
-						+ r.exception().getMessage() + " from object "
-						+ r.origin() + " (" + r.origin().getClass() + ")");
-			}
-		});
-		this.jsFactory = new JsFactory(this);
-		// jsFactory.getWrappers().addWrapper(OnedbWrapper.ONEJSON);
+        return exceptionManager;
+    }
 
-	}
+    @Override
+    public OnedbFactory getOnedbFactory() {
+        return new OnedbJsFactory();
+    }
 
-	@Override
-	public DefaultPluginFactory plugin() {
-		return H.onedbDefaultPluginFactory();
-	}
+    @Override
+    public Factory getFactory() {
+        return new OnedbJsFactory();
+    }
 
-	@Override
-	public JsFactory jsFactory() {
-		return jsFactory;
-	}
+    public OnedbNextwebJsEngineImpl() {
+        super();
+        this.exceptionManager = getOnedbFactory().createExceptionManager(null);
+        this.exceptionManager.catchExceptions(new ExceptionListener() {
 
-	@Override
-	public void runSafe(final Session forSession, final Runnable task) {
-		task.run(); // no multi-threading in JS assured.
-	}
+            @Override
+            public void onFailure(final ExceptionResult r) {
+                throw new RuntimeException("Unhandled exception: "
+                        + r.exception().getMessage() + " from object "
+                        + r.origin() + " (" + r.origin().getClass() + ")");
+            }
+        });
+        this.jsFactory = new JsFactory(this);
+        // jsFactory.getWrappers().addWrapper(OnedbWrapper.ONEJSON);
 
-	@Override
-	public boolean hasStartServerCapability() {
-		return startServerCapability != null;
-	}
+    }
 
-	@Override
-	public void injectCapability(final Capability capability) {
-		if (capability instanceof StartServerCapability) {
-			startServerCapability = (StartServerCapability) capability;
-			return;
-		}
+    @Override
+    public DefaultPluginFactory plugin() {
+        return H.onedbDefaultPluginFactory();
+    }
 
-		throw new IllegalArgumentException(
-				"This engine cannot recognize the capability: ["
-						+ capability.getClass() + "]");
-	}
+    @Override
+    public JsFactory jsFactory() {
+        return jsFactory;
+    }
 
-	@Override
-	public LocalServer startServer(final int port) {
-		if (startServerCapability == null) {
-			throw new IllegalStateException(
-					"Please inject a StartServerCapability first.");
-		}
+    @Override
+    public void runSafe(final Session forSession, final Runnable task) {
+        task.run(); // no multi-threading in JS assured.
+    }
 
-		return startServerCapability.startServer(port);
-	}
+    @Override
+    public boolean hasStartServerCapability() {
+        return startServerCapability != null;
+    }
+
+    @Override
+    public void injectCapability(final Capability capability) {
+        if (capability instanceof StartServerCapability) {
+            startServerCapability = (StartServerCapability) capability;
+            return;
+        }
+
+        throw new IllegalArgumentException(
+                "This engine cannot recognize the capability: ["
+                        + capability.getClass() + "]");
+    }
+
+    @Override
+    public LocalServer startServer(final int port) {
+        if (startServerCapability == null) {
+            throw new IllegalStateException(
+                    "Please inject a StartServerCapability first.");
+        }
+
+        return startServerCapability.startServer(port);
+    }
 
 }
