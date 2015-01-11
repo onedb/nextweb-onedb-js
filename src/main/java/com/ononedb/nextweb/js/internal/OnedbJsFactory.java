24a2aaa3010ffe18d30b49368b239505d806e1d8
mxro <mxro@nowhere.com>
2015-01-11 15:17:45 +1100

    Merging Promise Implementations Autocommit Sun Jan 11 15:17:45 EST 2015

--- src/main/java/com/ononedb/nextweb/js/internal/OnedbJsFactory.java
+++ src/main/java/com/ononedb/nextweb/js/internal/OnedbJsFactory.java
@@ -8,6 +8,8 @@ import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import com.ononedb.nextweb.common.OnedbFactory;
 import com.ononedb.nextweb.js.fn.JsResultImplementation;
 
+import de.mxro.promise.helper.Promise;
+
 public class OnedbJsFactory extends OnedbFactory {
 
     @Override
@@ -16,4 +18,9 @@ public class OnedbJsFactory extends OnedbFactory {
         return new JsResultImplementation<ResultType>(session, exceptionManager, asyncResult);
     }
 
+    @Override
+    public <ResultType> Promise<ResultType> createPromiseNew(final NextwebExceptionManager exceptionManager,
+            final Session session, final NextwebOperation<ResultType> asyncResult) {
+        throw new RuntimeException("Not supported yet.");
+    }
 }
