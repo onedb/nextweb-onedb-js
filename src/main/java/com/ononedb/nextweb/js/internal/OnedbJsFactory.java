524160918afe4d83e355b5071eab1fbdf6afa4ee
mxro <mxro@nowhere.com>
2015-10-28 13:01:30 +1100

    Implementing ObjectHub UI Autocommit Wed Oct 28 13:01:30 EST 2015

--- src/main/java/com/ononedb/nextweb/js/internal/OnedbJsFactory.java
+++ src/main/java/com/ononedb/nextweb/js/internal/OnedbJsFactory.java
@@ -10,18 +10,19 @@ import com.appjangle.api.Client;
 import com.ononedb.nextweb.common.DataPromiseImplWithClient;
 import com.ononedb.nextweb.common.OnedbFactory;
 
-import io.nextweb.promise.Fn;
 import io.nextweb.promise.DataOperation;
 import io.nextweb.promise.DataPromise;
+import io.nextweb.promise.Fn;
 import io.nextweb.promise.exceptions.DataExceptionManager;
 import io.nextweb.promise.utils.CallbackUtils;
 
 public class OnedbJsFactory extends OnedbFactory {
 
     @Override
-    public <ResultType> DataPromise<ResultType> createPromise(final DataExceptionManager exceptionManager,
+    public <ResultType> DataPromise<ResultType> createPromise(final DataExceptionManager fallbackExceptionManager,
             final Client session, final DataOperation<ResultType> asyncResult) {
 
+        final DataExceptionManager exceptionManager = createExceptionManager(fallbackExceptionManager);
         final Promise<ResultType> promise = createPromiseNew(exceptionManager, session, asyncResult);
 
         return new DataPromiseImplWithClient<ResultType>(asyncResult, promise, exceptionManager, session);
